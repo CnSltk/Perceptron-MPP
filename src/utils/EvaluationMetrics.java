d99ff5d7580aaced0e3e07d8017785b17a234951
CnSltk <cansaltik1903@gmail.com>
2025-03-31 01:39:12 +0200

    added the EvaluationMetrics.java class

--- src/utils/EvaluationMetrics.java
+++ src/utils/EvaluationMetrics.java
@@ -1,5 +1,16 @@
 package utils;
 
 public class EvaluationMetrics {
-
+    public static double measureAccuracy(int[] trueLabels, int[] predictedLabels){
+        if(trueLabels.length != predictedLabels.length){
+            throw new IllegalArgumentException("Array must be the same length");
+        }
+        int correct = 0;
+        for (int i = 0; i < trueLabels.length; i++) {
+            if(trueLabels[i] == predictedLabels[i]){
+             correct++;
+            }
+        }
+        return (double) correct / trueLabels.length;
+    }
 }
